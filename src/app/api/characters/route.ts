f65d039f7c031187cffb9e33d9590bb106087516
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-10-09 10:50:33 -0400

    add search params for characters

--- src/app/api/characters/route.ts
+++ src/app/api/characters/route.ts
@@ -26,3 +26,48 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ success: false, error: error });
   }
 }
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url)
+  const limit = parseInt(searchParams.get('limit') || '10')
+  const offset = parseInt(searchParams.get('offset') || '0')
+
+  const idSearch = searchParams.get('id')
+  const nameSearch = searchParams.get('name')
+  const episodeSearch = searchParams.get('episode')
+
+  const filters: any = {}
+
+  if (idSearch) {
+    filters.id = parseInt(idSearch)
+  }
+
+  if (nameSearch) {
+    filters.name = { contains: nameSearch, mode: 'insensitive' }
+  }
+
+  if (episodeSearch) {
+    filters.episodes = {
+      some: {
+        name: {
+          contains: episodeSearch,
+          mode: 'insensitive'
+        }
+      }
+    }
+  }
+
+  try {
+    const characters = await prisma.character.findMany({
+      where: filters,
+      skip: offset,
+      take: limit,
+      include: {
+        episodes: true,
+      },
+    });
+    return NextResponse.json({ success: true, data: characters });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error });
+  }
+}
